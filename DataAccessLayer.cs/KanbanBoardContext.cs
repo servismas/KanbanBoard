964dbbc6e8269874ccde6dd10eae47f93007401c
servismas <servismas@gmail.com>
2019-05-29 20:06:48 +0300

    Added PresentationLayer

--- DataAccessLayer.cs/KanbanBoardContext.cs
+++ DataAccessLayer.cs/KanbanBoardContext.cs
@@ -8,20 +8,11 @@
 
     public class KanbanBoardContext : DbContext
     {
-        // Контекст настроен для использования строки подключения "KanbanBoardContext" из файла конфигурации  
-        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
-        // "DataAccessLayer.cs.KanbanBoardContext" в экземпляре LocalDb. 
-        // 
-        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "KanbanBoardContext" 
-        // в файле конфигурации приложения.
         public KanbanBoardContext()
             : base("name=KanbanBoardContext")
         {
             Database.SetInitializer(new DbInitializer());
         }
-
-        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
-        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
 
          public virtual DbSet<User> Users { get; set; }
          public virtual DbSet<Team> Teams { get; set; }
@@ -30,9 +21,4 @@
          public virtual DbSet<Board> Boards { get; set; }
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
