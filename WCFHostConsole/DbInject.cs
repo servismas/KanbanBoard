938e3f9a6ea2d893f14081dbfc0db390b3e44139
Володимир Сологубов <wova607@hotmail.com>
2019-06-01 23:08:20 +0300

    ADD ninJect

---
+++ WCFHostConsole/DbInject.cs
@@ -0,0 +1,20 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.cs.Interfases;
+using DataAccessLayer.cs.Repository;
+using DataAccessLayer.cs;
+
+namespace WCFHostConsole
+{
+    public class DbInject : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind(typeof(IRepository<>)).To(typeof(Repository<>)).WithConstructorArgument("_context", new KanbanBoardContext());
+        }
+    }
+}
