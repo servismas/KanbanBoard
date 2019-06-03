80a6b413d8b13b3acbd30a3614469a68619c9e68
Володимир Сологубов <wova607@hotmail.com>
2019-06-04 00:00:22 +0300

    ADDNewContracts

---
+++ WcfBussinesLogicLayerLibrary/Contracts/ICreateEditAttachmentContract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfBussinesLogicLayerLibrary.ModelsDTO;
+
+namespace WcfBussinesLogicLayerLibrary.Contracts
+{
+    [ServiceContract]
+    interface ICreateEditAttachmentContract
+    {
+        [OperationContract]
+        void AddNewAttachment(AttachmentDTO newAtachment);
+        [OperationContract]
+        void DeleteAtachment(AttachmentDTO deleteAtachment);
+        [OperationContract]
+        List<AttachmentDTO> GetAttachments(CardDTO card);
+        [OperationContract]
+        AttachmentDTO GetAttachment(Attachment attachment);
+
+
+    }
+}
---
+++ WcfBussinesLogicLayerLibrary/Contracts/ICreateEditeProfileContract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfBussinesLogicLayerLibrary.ModelsDTO;
+
+namespace WcfBussinesLogicLayerLibrary.Contracts
+{
+    [ServiceContract]
+    interface ICreateEditeProfileContract
+    {
+        [OperationContract]
+        void AddProfile(ProfileDTO newProfile);
+        [OperationContract]
+        void EditeProfile(ProfileDTO editeProfile);
+        [OperationContract]
+        ProfileDTO GetProfileDTO(Profile profile);
+        [OperationContract]
+        Profile GetProfile(ProfileDTO profileDTO);
+
+    }
+}
